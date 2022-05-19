6f6f4678024566860ae7ba81910addd0b17d3c58
wnsgml0440 <wnsgml0440@naver.com>
2022-05-19 18:17:47 +0900

    2022년 5월 19일 JavaScript 활용

---
+++ JS project/js/greetings.js
@@ -0,0 +1,30 @@
+const loginForm = document.querySelector("#login-form")
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const Hidden_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(Hidden_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(Hidden_CLASSNAME);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(Hidden_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
